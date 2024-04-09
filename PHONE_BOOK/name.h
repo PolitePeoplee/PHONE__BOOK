09bc54f65d2c5b2659e1a54236240881e458128f
PolitePeoplee <150465337+PolitePeoplee@users.noreply.github.com>
2024-04-09 12:57:50 +0300

    Create name.h

---
+++ PHONE_BOOK/name.h
@@ -0,0 +1,11 @@
+//﻿#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <string> 
+#include <cassert>
+#include <vector>
+#include <cstring>
+vector<int> found(string find_name);
+void showuserdata(vector<int> nums);
+bool only_digits(const char* phone_number);
+bool only_letters(const char* name);
