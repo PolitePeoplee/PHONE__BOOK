19adeb7c650ebbc3e16468525ac0715d0e9cdaf4
PolitePeoplee <150465337+PolitePeoplee@users.noreply.github.com>
2024-04-09 16:17:46 +0300

    Update user.h

--- PHONE_BOOK/user.h
+++ PHONE_BOOK/user.h
@@ -1,9 +1,18 @@
 #include<string>
-struct User
+
+struct PHONE_BOOK
 {
+private:
     char* first_name;
     char* last_name;
     char* father_s_name;
     char* phone_number;
     string find_family;
+public:
+PHONE_BOOK();
+void write_to_file();
+void enter_fio();
+void enter_number();
+void enter_find_word();
+~PHONE_BOOK();
 }
